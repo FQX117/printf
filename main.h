117e8f31d45b6d4189fcc0f73aa2000ac076c43e
FQX117 <91921154+FQX117@users.noreply.github.com>
2022-03-01 11:17:10 -0600

    Update main.h

--- main.h
+++ main.h
@@ -1,6 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
+/* Headers*/
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdarg.h>
+
+/*Prototypes*/
+int _putchar (char c);
 int _printf(const char *format, ...);
+
 #endif
